fba63de256ebf39a898ea6494a0d4c73ad829a63
Fumi KASAI <hiiragi390@gmail.com>
2023-01-03 15:16:32 +0900

    天気予報サイト追加

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ closeButton1.addEventListener("click", {n:1, handleEvent:close});
 closeButton2.addEventListener("click", {n:2, handleEvent:close});
 closeButton3.addEventListener("click", {n:3, handleEvent:close});
 closeButton4.addEventListener("click", {n:4, handleEvent:close});
-//closeButton5.addEventListener("click", {n:5, handleEvent:close});
+closeButton5.addEventListener("click", {n:5, handleEvent:close});
 closeButton6.addEventListener("click", {n:6, handleEvent:close});
 //closeButton7.addEventListener("click", {n:7, handleEvent:close});
 
@@ -237,7 +237,7 @@ openButton1.addEventListener("click", {n:1, handleEvent:open});
 openButton2.addEventListener("click", {n:2, handleEvent:open});
 openButton3.addEventListener("click", {n:3, handleEvent:open});
 openButton4.addEventListener("click", {n:4, handleEvent:open});
-//openButton5.addEventListener("click", {n:5, handleEvent:open});
+openButton5.addEventListener("click", {n:5, handleEvent:open});
 openButton6.addEventListener("click", {n:6, handleEvent:open});
 //openButton7.addEventListener("click", {n:7, handleEvent:open});
 
